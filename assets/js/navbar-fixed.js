67732e7d3089b0c3a52c78639f16e802fb07955f
Diego Peroza <diegoeleveth@gmail.com>
2024-03-08 18:31:31 +0100

    Comprobar scroll

---
+++ assets/js/navbar-fixed.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let navbar = document.querySelector(".menu__navbar");
+
+    window.addEventListener("scroll", () =>{
+        console.log(window.scrollY);
+    });
+});
