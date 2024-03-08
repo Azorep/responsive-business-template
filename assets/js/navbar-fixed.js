541af790a5b8f8f915a4da1f7b021de0cb7a73eb
Diego Peroza <diegoeleveth@gmail.com>
2024-03-08 18:53:28 +0100

    JavaScript header sticky

--- assets/js/navbar-fixed.js
+++ assets/js/navbar-fixed.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     let navbar = document.querySelector(".menu__navbar");
 
     window.addEventListener("scroll", () =>{
-        console.log(window.scrollY);
+        let scroll = window.scrollY;
+
+        if(scroll > 120){
+            navbar.classList.add("menu__navbar--fixed");
+        }else{
+            navbar.classList.remove("menu__navbar--fixed");
+        }
     });
 });
